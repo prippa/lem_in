4f3b07e0b8c823df3d4112cec0ef5bc1f68afa09
prippa <pavelrippa@gmail.com>
2018-02-16 16:26:14 +0200

    base version of lem_in project

---
+++ includes/lem_in.h
@@ -0,0 +1,7 @@
+#ifndef LEM_IN_H
+# define LEM_IN_H
+
+# include "libft.h"
+# include "ft_printf.h"
+
+#endif
---
+++ source/main.c
@@ -0,0 +1,7 @@
+#include "lem_in.h"
+
+int		main(int argc, char **argv)
+{
+	ft_printf("Hello from school >>>%~d<<<\n", F_MAGENTA, 42);
+	return (0);
+}
